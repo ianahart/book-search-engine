40d6da846ba4231e7e42eb7de3a26fedfed5c8a7
Ian Hart <ianhart7790@gmail.com>
2024-05-17 16:05:57 -0400

    feat: change proxy url on the client to production url

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/graphql': {
-        target: 'http://localhost:3001',
+        target: 'https://book-search-engine-ftr1.onrender.com',
         secure: false,
         changeOrigin: true,
       },
