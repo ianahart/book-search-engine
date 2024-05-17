05380c7eaf44cf40567b6ca0f998b73de8d53eec
Ian Hart <ianhart7790@gmail.com>
2024-05-17 11:04:06 -0400

    feat: add book type to schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,12 +1,20 @@
 const typeDefs = `
     type User {
-         _id: ID
+         _id: ID!
         username: String!
         email: String!
         bookCount: Integer
         savedBooks: [Book]
     }
 
+    type Book {
+       bookId: ID!
+       authors: [String]
+       description: String!
+       title: String
+       image String
+       link String
+    }
 `;
 
 module.exports = typeDefs;
